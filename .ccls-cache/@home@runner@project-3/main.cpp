94de20a47ae1d5cf0e73c32442c8b0514639b0b2
Dione099 <dionegeorge.dg@gmail.com>
2024-11-14 15:33:07 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@project-3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //the original amounts of each ingredient.
+const double sugarPer48 = 1.5;
+const double butterPer48 = 1.0;
+const double flourPer48 = 2.75;
+const int cookiesPerBatch = 48;
+
+  int desiredCookies;
+  cout << "How many cookies do you want to make:";
+  cin >> desiredCookies;
+  double ratio = static_cast<double>(desiredCookies) / cookiesPerBatch;
+  
+  double sugarNeeded = (sugarPer48 / cookiesPerBatch) * desiredCookies;
+  double butterNeeded = (butterPer48 / cookiesPerBatch) * desiredCookies;
+  double flourNeeded = (flourPer48 / cookiesPerBatch) * desiredCookies;
+
+  cout << "To make " << desiredCookies << " cookies, you will need:\n";
+  cout << sugarNeeded << " cups of sugar\n";
+  cout << butterNeeded << " cups of butter\n";
+  cout << flourNeeded << " cups of flour\n";
+  return 0;
+}
