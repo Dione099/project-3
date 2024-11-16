1d046f71124d702ef5c8edc304b01bd72d851da8
Dione099 <dionegeorge.dg@gmail.com>
2024-11-16 04:26:04 +0000

    starting problem 3

--- .ccls-cache/@home@runner@project-3/main.cpp
+++ .ccls-cache/@home@runner@project-3/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-  
+int main() {
+  //declare variables for pricipal, interest rate, and time
+  double principal, interestRate, time;
+  int timesCompounded;
+}
--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-  //put two numbers needed to add
- int num1=247;
-  int num2=129;
-
- std:: cout<< "What is the sum" << num1 << " + " << num2 << "? " << endl;
- 
-  //user would now solve problem
-  std::cout<<"Press any key to see the answer" << std::endl;
-    std::cin.get();
-  //display the answer
-  int answer = num1 + num2;
-  std::cout <<"The correct answer is " << answer << endl;
-
-    return 0;
+int main() {
+  //declare variables for pricipal, interest rate, and time
+  double principal, interestRate, time;
+  int timesCompounded;
 }
