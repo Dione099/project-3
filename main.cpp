51a4f5b61165033db5d37802faa0616e44962dc0
Dione099 <dionegeorge.dg@gmail.com>
2024-11-16 03:43:20 +0000

    random number generator

--- main.cpp
+++ main.cpp
@@ -2,4 +2,21 @@
 using namespace std;
 
 int main(){
-  
+  //initiaize the random number generator
+ sran(time(0)); 
+
+  //generate two random numbers between 1 and 999
+  int num1 = rand() % 10 + 1;
+  int num2 = rand() % 10 + 1;
+
+  cout<< "What is " << num1 << " + " << num2 << "? " << endl;
+
+  //user would now solve problem
+  cout<<"Press any key to see the answer" << endl;"
+    cin.get();
+  //display the answer
+  int answer = num1 + num2;
+  cout <<"The correct answer is " << answer << endl;"
+
+    return 0;
+}
