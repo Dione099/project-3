74c075ec266c1a5c8c31fca6a55ddae5bfbc8309
Dione099 <dionegeorge.dg@gmail.com>
2024-11-19 14:58:04 +0000

    figuring this out with tutor

--- .ccls-cache/@home@runner@project-3/main.cpp
+++ .ccls-cache/@home@runner@project-3/main.cpp
@@ -4,4 +4,22 @@ int main() {
   //declare variables for pricipal, interest rate, and time
   double principal, interestRate, time;
   int timesCompounded;
+  //what is the principal
+  std::cout << "What is the principal? ";
+  std::cin >> principal;
+
+  //what is the interest rate
+  std::cout << "What is the interest rate? ";
+  std::cin >> interestRate;
+
+  //what is the time
+  std::cout << "What is the time? ";
+  std::cin >> time;
+
+ rate = rate / 100;
+
+  //what is the total amount in the account after 1 year?
+  std::cout << "What is the total amount in the account after 1 year? ";
+  std::cin >> TotalAmount;
+  
 }
